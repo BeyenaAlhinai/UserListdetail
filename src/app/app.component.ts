c3c5126d5bacdc715c7479f2287da10628d4da77
BeyenaAlhinai <Beyenaalhinai@gmail.com>
2023-10-15 16:18:38 +0400

    userlist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Userlist';
+  isDialogOpen = false;
+  selectedUserName: string | null = null;
+
+  openDialog(userName: string) {
+    this.selectedUserName = userName;
+    this.isDialogOpen = true;
+  }
+
+  closeDialog() {
+    this.isDialogOpen = false;
+    this.selectedUserName = null;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MegaMenuModule } from 'primeng/megamenu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MegaMenuModule,
+    PanelMenuModule,
+    DialogModule,
+    DropdownModule,
+    FormsModule,
+    ButtonModule,
+    ReactiveFormsModule
+   
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
